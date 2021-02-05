12118fb062433646e74f6cbb238d5acb21cc8cfd
Roman Pinelo <prof.roman.pinelo@gmail.com>
2021-02-05 16:12:13 -0600

    Página terminada

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# raices_ecuacion
+# Raíces de una ecuación cuadrática. 
+
+Tomando como base la práctica de "Dibujando en canvas", se piden los datos de a, b y c de una ecuación cuadrática. Una vez introducidos se calculan las raíces y se grafican en un canvas.
+
+
+## Construido con 🛠️
+
+* HTML
+* CSS
+* JS
+
+
+## Autores ✒️
+
+José Román Pinelo Escobar
+
+
+### Explicación
+
+Esta es la pantalla principal donde, en tres input se pide el valor de a, b y c de una ecuación de segundo grado. En los inputs se agrego una animación y son de tipo numérico:
+
+![1](https://user-images.githubusercontent.com/71656431/107093040-32e73500-67ca-11eb-819c-4534a051f35e.jpg)
+
+
+
+Una vez ingresados los datos, hacemos click en el botón 'Calcular raíces' y mostrará los resultados y a la vez graficará en el canvas:
+
+![2](https://user-images.githubusercontent.com/71656431/107093891-b48b9280-67cb-11eb-892a-91402ba5b2d2.jpg)
+
+
+
+Si modificamos los datos en los input y damos click en 'Calcular raíces', mostrará los nuevos resultados, borrando previamente los anteriores. Tambien en el canvas se borrará la gráfica anterior y se dibujará la nueva:
+
+![3](https://user-images.githubusercontent.com/71656431/107094145-2532af00-67cc-11eb-9693-35834d49cc91.jpg)
+
+
+
+También está la opción de hacer click en el botón 'Hacer otro cálculo' y borrará los resultados, lo que pusimos en los input y la gráfica:
+
+![4](https://user-images.githubusercontent.com/71656431/107094519-c02b8900-67cc-11eb-8f6f-c149a32dce59.jpg)
+
+En los botones también se pusieron efectos hover.
+
+
+
+En el siguiente link puedes ver la página funcionando: 
+
+https://romanpinelo.github.io/
---
+++ calculo.js
@@ -0,0 +1,152 @@
+/**********************************************************************
+
+    TOMAR LOS DATOS DE LOS INPUT Y CALCULAR LAS RAÍCES.
+
+**********************************************************************/
+/* Datos de a, b y c */
+var valora = document.getElementById("valorA");
+var valorb = document.getElementById("valorB");
+var valorc = document.getElementById("valorC");
+
+
+/* Botón de calcular */
+var boton = document.getElementById("botonCalcular");
+boton.addEventListener("click", calcularRaices );
+
+function calcularRaices() {
+  var a = valora.value;
+  var b = valorb.value;
+  var c = valorc.value;
+  
+  var x1,x2; //Raíces
+  
+  var xi, yi, xf, yf; //Coordenadas
+  var i; //Ciclo for
+  
+  var radicando = (Math.pow(b, 2)) - (4 * a * c);
+  
+  
+  //Borrando el canvas
+  lienzo.clearRect(0, 0, d.width, d.height);
+  
+  //Dibujando ejes y divisiones.
+  dibujarLinea("red", 149, 1, 149, 299); //Eje x
+  dibujarLinea("red", 1, 149, 299, 149); //Eje y
+  
+  for(i=0; i <= 30; i++){ //Divisiones eje x
+    xi = 10 * (i);
+    yi = 144;
+    xf = xi;
+    yf = 154;
+    dibujarLinea("red", xi, yi, xf, yf);
+  }
+  
+  for(i=0; i <= 30; i++){  //Divisiones eje y
+    xi = 144;
+    yi = 10 * (i);
+    xf = 154;
+    yf = yi;
+    dibujarLinea("red", xi, yi, xf, yf);
+  }
+  
+  
+  if(radicando == 0){
+    x1 = (-1 * b) / (2 * a);
+    
+    document.getElementById('ecuacion').innerHTML = a + "x^2 + " + b + "x + " + c + " = 0";
+    document.getElementById('valorx1').innerHTML = "El valor de ambas raíces es: " + x1;
+    document.getElementById('valorx2').innerHTML = "";
+    
+    xi = 150 + (x1 * 10);
+    yi = 0;
+    xf = xi;
+    yf = 300;
+    dibujarLinea("#2196f3", xi, yi, xf, yf);
+  }
+  
+  else if(radicando > 0){
+    x1 = ((-1 * b) + Math.sqrt(radicando)) / (2 * a);
+    x2 = ((-1 * b) - Math.sqrt(radicando)) / (2 * a);
+    
+    document.getElementById('ecuacion').innerHTML = a + "x^2 + " + b + "x + " + c + " = 0";
+    document.getElementById('valorx1').innerHTML = "El valor de x1 es: " + x1.toFixed(2);
+    document.getElementById('valorx2').innerHTML = "<br>El valor de x2 es: " + x2.toFixed(2);
+    
+    xi = 150 + (x1 * 10);
+    yi = 0;
+    xf = xi;
+    yf = 300;
+    dibujarLinea("#2196f3", xi, yi, xf, yf);
+    
+    xi = 150 + (x2 * 10);
+    yi = 0;
+    xf = xi;
+    yf = 300;
+    dibujarLinea("cyan", xi, yi, xf, yf);
+  }
+  
+  else {
+    //Borrando el canvas
+    lienzo.clearRect(0, 0, d.width, d.height);
+    //Escribinedo texto
+    lienzo.font="bold 15px arial";
+    lienzo.fillStyle="#FFF";
+    lienzo.fillText("En realidad el radicando daba: " + radicando + ", ", 10, 20);
+    lienzo.font="bold 15px arial";
+    lienzo.fillStyle="#FFF";
+    lienzo.fillText("pero se convirtió a positivo. Disculpe ", 10, 40);
+    lienzo.font="bold 15px arial";
+    lienzo.fillStyle="#FFF";
+    lienzo.fillText("las molestias.", 10, 60);
+    
+    radicando = -1 * radicando;
+    x1 = ((-1 * b) + Math.sqrt(radicando)) / (2 * a);
+    x2 = ((-1 * b) - Math.sqrt(radicando)) / (2 * a);
+    
+    document.getElementById('ecuacion').innerHTML = a + "x^2 + " + b + "x + " + c + " = 0";
+    document.getElementById('valorx1').innerHTML = "El valor de x1 es: " + x1.toFixed(2);
+    document.getElementById('valorx2').innerHTML = "<br>El valor de x2 es: " + x2.toFixed(2);
+  }
+}
+
+
+
+/*************************************************************************************
+
+    BORRAR LOS DATOS DE LOS INPUTS Y LOS PÁRRAFOS DONDE SE IMPRIMEN LOS RESULTADOS.
+
+*************************************************************************************/
+/* Botón de borrar */
+var boton = document.getElementById("botonBorrar");
+boton.addEventListener("click", borrarDatos);
+
+function borrarDatos() {
+  lienzo.clearRect(0, 0, d.width, d.height);
+  document.getElementById('ecuacion').innerHTML = "";
+  document.getElementById('valorx1').innerHTML = "";
+  document.getElementById('valorx2').innerHTML = "";
+  document.getElementById('valorA').value = "";
+  document.getElementById('valorB').value = "";
+  document.getElementById('valorC').value = "";
+}
+
+
+/**********************************************************************************
+
+    DIBUJAR EN EL CANVAS
+
+**********************************************************************************/
+/* Datos del canvas */
+var d = document.getElementById("plano");
+var lienzo = d.getContext("2d");
+
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 2;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
